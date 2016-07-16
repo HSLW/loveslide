79d46c6652589cef9000009392e750ce155bbb0e
Ed Choi <ed.choi@buzzvil.com>
2016-07-16 20:02:26 +0900

    added more fields to return

--- lockscreen/serializers.py
+++ lockscreen/serializers.py
@@ -4,4 +4,4 @@ from rest_framework import serializers
 class CardSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Card
-        fields = ('name', 'image_url', 'age')
+        fields = ('name', 'image_url', 'age', 'height', 'job', 'region', 'message')
