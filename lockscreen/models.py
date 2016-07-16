4d2c1c1575400936a02471ab82ebea3f1d09a7a4
Ed Choi <ed.choi@buzzvil.com>
2016-07-16 16:45:21 +0900

    added Card model

---
+++ lockscreen/models.py
@@ -0,0 +1,15 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=50)
+    image_url = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
+    height = models.IntegerField(default=0)
+    region = models.CharField(max_length=50)
+    job = models.CharField(max_length=50)
+    message = models.CharField(max_length=255)
