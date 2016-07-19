44ee4807029da402ca8f4a7a29c0952f242147fb
Ed Choi <ed.choi@buzzvil.com>
2016-07-19 21:21:43 +0900

    wip

--- relation/management/commands/ed.py
+++ relation/management/commands/ed.py
@@ -5,9 +5,9 @@ from relation.models import Like
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        user1 = User.objects.filter(id=1)
-        user2 = User.objects.filter(id=2)
-        user3 = User.objects.filter(id=3)
+        user1 = User.objects.get(id=1)
+        user2 = User.objects.get(id=2)
+        user3 = User.objects.get(id=3)
         like1 = Like.objects.create(user1=user1, user2=user2)
         like2 = Like.objects.create(user1=user3, user2=user2)
         like3 = Like.objects.create(user1=user2, user2=user1)
