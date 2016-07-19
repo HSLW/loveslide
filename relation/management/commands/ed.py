eb4b242e46a4075400cf8415c369b4edde48f303
Ed Choi <ed.choi@buzzvil.com>
2016-07-19 21:17:03 +0900

    command for like

--- relation/management/commands/ed.py
+++ relation/management/commands/ed.py
@@ -1,12 +1,16 @@
+from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 from relation.models import Like
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        like1 = Like.objects.create(user1=2, user2=1)
-        like2 = Like.objects.create(user1=3, user2=1)
-        like3 = Like.objects.create(user1=4, user2=1)
+        user1 = User.objects.filter(id=1)
+        user2 = User.objects.filter(id=2)
+        user3 = User.objects.filter(id=3)
+        like1 = Like.objects.create(user1=user1, user2=user2)
+        like2 = Like.objects.create(user1=user3, user2=user2)
+        like3 = Like.objects.create(user1=user2, user2=user1)
         like1.save()
         like2.save()
         like3.save()
